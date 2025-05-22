0d9f19650c40c0efbc0260b5e2b2101f9268df1f
anilreddytirugudu <anil.tirugudu@kornferry.com>
2025-05-22 17:07:20 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,22 +5,15 @@
 
 java -jar /flow-diff.jar $1 $2 >> /github/workspace/diff.txt
 
-OUTPUT=$(cat /github/workspace/diff.txt | sed 's/"/\\"/g' | sed ':a;N;$!ba;s/\n/\\n/g')
-JSON_PAYLOAD_FILE="/github/workspace/payload.json"
-printf '{"body":"%s"}' "$(cat /github/workspace/diff.txt | sed 's/"/\\"/g' | sed ':a;N;$!ba;s/\n/\\n/g')" > "$JSON_PAYLOAD_FILE"
-curl -X POST \
-    -H "Authorization: Token $3" \
-    -H "Accept: application/vnd.github+json" \
-    "https://api.github.com/repos/$4/issues/$5/comments" \
-    -d "@$JSON_PAYLOAD_FILE"
+#OUTPUT=$(cat /github/workspace/diff.txt | sed 's/"/\\"/g' | sed ':a;N;$!ba;s/\n/\\n/g')
+
 
 # Clean up the temporary JSON payload file
-rm "$JSON_PAYLOAD_FILE"
 
-#curl -X POST \
-#    -H "Authorization: Token $3" \
-#   -H "Accept: application/vnd.github+json" \
-#     https://api.github.com/repos/$4/issues/$5/comments \
-#     -d "{\"body\":\"$OUTPUT\"}"
+curl -X POST \
+    -H "Authorization: Token $3" \
+   -H "Accept: application/vnd.github+json" \
+     https://api.github.com/repos/$4/issues/$5/comments \
+     -d "{\"body\":\"$(cat /github/workspace/diff.txt | sed 's/"/\\"/g' | sed ':a;N;$!ba;s/\n/\\n/g')\"}"
 
 
