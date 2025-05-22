259b7c2f7856f9e84585ae445bf5599fd5bb9778
anilreddytirugudu <anil.tirugudu@kornferry.com>
2025-05-22 16:52:07 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,8 @@ java -jar /flow-diff.jar $1 $2 >> /github/workspace/diff.txt
 
 OUTPUT=$(cat /github/workspace/diff.txt | sed 's/"/\\"/g' | sed ':a;N;$!ba;s/\n/\\n/g')
 
-curl -X POST \
-    -H "Authorization: Token $3" \
-   -H "Accept: application/vnd.github+json" \
-     https://api.github.com/repos/$4/issues/$5/comments \
-     -d "{\"body\":\"$OUTPUT\"}"
+#curl -X POST \
+#    -H "Authorization: Token $3" \
+#   -H "Accept: application/vnd.github+json" \
+#     https://api.github.com/repos/$4/issues/$5/comments \
+#     -d "{\"body\":\"$OUTPUT\"}"
