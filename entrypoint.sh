3ccb99bd7d3a3c99275d041ae6e1378667f7b0d1
anilreddytirugudu <anil.tirugudu@kornferry.com>
2025-05-22 17:03:23 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,16 @@
 java -jar /flow-diff.jar $1 $2 >> /github/workspace/diff.txt
 
 OUTPUT=$(cat /github/workspace/diff.txt | sed 's/"/\\"/g' | sed ':a;N;$!ba;s/\n/\\n/g')
+JSON_PAYLOAD_FILE="/github/workspace/payload.json"
+printf '{"body":"%s"}' "$(cat /github/workspace/diff.txt | sed 's/"/\\"/g' | sed ':a;N;$!ba;s/\n/\\n/g')" > "$JSON_PAYLOAD_FILE"
+curl -X POST \
+    -H "Authorization: Token $3" \
+    -H "Accept: application/vnd.github+json" \
+    "https://api.github.com/repos/$4/issues/$5/comments" \
+    -d "@$JSON_PAYLOAD_FILE"
+
+# Clean up the temporary JSON payload file
+rm "$JSON_PAYLOAD_FILE"
 
 #curl -X POST \
 #    -H "Authorization: Token $3" \
@@ -13,9 +23,4 @@ OUTPUT=$(cat /github/workspace/diff.txt | sed 's/"/\\"/g' | sed ':a;N;$!ba;s/\n/
 #     https://api.github.com/repos/$4/issues/$5/comments \
 #     -d "{\"body\":\"$OUTPUT\"}"
 
-#curl -r PUT -u "$user:$token" -d "$(jo message=update content=@local_file sha=$github_hash)" https://api.github.com/repos/$user/$repo/contents/$file 
-curl -X POST \
-    -H "Authorization: Token $3" \
-   -H "Accept: application/vnd.github+json" \
-     https://api.github.com/repos/$4/issues/$5/comments \
-     -d "$(jo body=$(cat /github/workspace/diff.txt | sed 's/"/\\"/g' | sed ':a;N;$!ba;s/\n/\\n/g'))"
+
