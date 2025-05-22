16b0c654995afdc5733ab3f76a7f4f7feb04b7b4
anilreddytirugudu <anil.tirugudu@kornferry.com>
2025-05-22 16:56:38 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,3 +12,10 @@ OUTPUT=$(cat /github/workspace/diff.txt | sed 's/"/\\"/g' | sed ':a;N;$!ba;s/\n/
 #   -H "Accept: application/vnd.github+json" \
 #     https://api.github.com/repos/$4/issues/$5/comments \
 #     -d "{\"body\":\"$OUTPUT\"}"
+
+#curl -r PUT -u "$user:$token" -d "$(jo message=update content=@local_file sha=$github_hash)" https://api.github.com/repos/$user/$repo/contents/$file 
+curl -X POST \
+    -H "Authorization: Token $3" \
+   -H "Accept: application/vnd.github+json" \
+     https://api.github.com/repos/$4/issues/$5/comments \
+     -d "$(jo message=update content=$(cat /github/workspace/diff.txt | sed 's/"/\\"/g' | sed ':a;N;$!ba;s/\n/\\n/g') sha=$github_hash)"
