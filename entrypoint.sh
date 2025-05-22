91779151f127e28db1899c4d89a2e685943b21f8
anilreddytirugudu <anil.tirugudu@kornferry.com>
2025-05-22 17:12:09 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,10 +10,21 @@ java -jar /flow-diff.jar $1 $2 >> /github/workspace/diff.txt
 
 # Clean up the temporary JSON payload file
 
+#curl -X POST \
+#    -H "Authorization: Token $3" \
+#   -H "Accept: application/vnd.github+json" \
+#     https://api.github.com/repos/$4/issues/$5/comments \
+#     -d "{\"body\":\"$(cat /github/workspace/diff.txt | sed 's/"/\\"/g' | sed ':a;N;$!ba;s/\n/\\n/g')\"}"
+
+
+echo "{\"body\":\"$(cat /github/workspace/diff.txt | sed 's/"/\\"/g' | sed ':a;N;$!ba;s/\n/\\n/g')\"}" > /tmp/github_comment_payload.json
+
+# Send the POST request, reading the payload from the temporary file
 curl -X POST \
-    -H "Authorization: Token $3" \
-   -H "Accept: application/vnd.github+json" \
-     https://api.github.com/repos/$4/issues/$5/comments \
-     -d "{\"body\":\"$(cat /github/workspace/diff.txt | sed 's/"/\\"/g' | sed ':a;N;$!ba;s/\n/\\n/g')\"}"
+  -H "Authorization: Token $3" \
+  -H "Accept: application/vnd.github+json" \
+  "https://api.github.com/repos/$4/issues/$5/comments" \
+  -d @/tmp/github_comment_payload.json
 
-
+# Clean up the temporary JSON payload file
+rm /tmp/github_comment_payload.json
